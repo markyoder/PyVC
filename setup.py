85c3a7da9128aac4083dc8e449c9725282e41706
mark Yoder <mark.yoder@gmail.com>
2014-10-23 16:26:10 -0700

    adding setup.py distutils script...

---
+++ setup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python
+
+from distutils.core import setup
+
+setup(name='PyVC',
+	version='1.0',
+	description='Python tools for Virtual Quake/California analysis',
+	author='mark yoder',
+	author_email='mryoder@ucdavis.edu',
+	packages=['pyvc'],
+	)
